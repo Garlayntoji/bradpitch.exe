cbae4403cb04df1758d2bd248d9e171bac8e692a
Garlayntoji <gogo010702@gmail.com>
2020-08-25 01:33:16 +0200

    Created new command info. Edited help.

--- commands/help.js
+++ commands/help.js
@@ -10,17 +10,15 @@ module.exports = {
 
 		const helpEmbed = new Discord.MessageEmbed()
 			.setColor('#c75a00')
-			.setTitle('Help')
-			.setAuthor('Garlayn Toji (click on me!)', 'https://cdn.discordapp.com/avatars/266216852785594379/76c550744bcef3f7caf665c90bb6d9cb.png', 'https://discord.gg/stSuDjS')
+			.setTitle('Help: General')
 			.setDescription("Here's the help page. Thank you for using Brad Pitch")
-			.setThumbnail('https://cdn.discordapp.com/avatars/266216852785594379/76c550744bcef3f7caf665c90bb6d9cb.png')
+			.setThumbnail('https://media.discordapp.net/attachments/489384737752875009/651385255525351425/thumbs.jpg')
 			.addFields(
 				{ name: 'b!help', value: 'You already know the result.' },
 				{ name: 'b!ping', value: 'Brad Pitch responds "Pong".' },
-				{ name: 'Work in progress', value: 'Work in progress' },
+				{ name: 'b!info', value: 'Shows the informations page.' },
 			)
 			.addField('Work in progress', 'Work in progress')
-			.setImage('https://media.discordapp.net/attachments/489384737752875009/651385255525351425/thumbs.jpg')
 			.setTimestamp()
 			.setFooter('Brad Pitch.exe', 'https://media.discordapp.net/attachments/489384737752875009/651385255525351425/thumbs.jpg');
 
